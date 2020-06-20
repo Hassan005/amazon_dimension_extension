16e910d0780b0f21c95710ac0932f557bcfddb7d
Hassan Asghar <30577649+Hassan005@users.noreply.github.com>
2020-06-20 13:16:38 +0500

    extracting shiping weight

--- background.js
+++ background.js
@@ -2,9 +2,11 @@ console.log("bsckground running")
 
 chrome.runtime.onMessage.addListener(receiver);
 window.measurements="Hello There"
+window.item_weight=" N/A"
 function receiver(request,sender,sendResponse)
 {
     
     window.measurements=request
+    console.log(request,"request*************************")
     // console.log(window.title)
 }
--- content.js
+++ content.js
@@ -19,26 +19,68 @@
    }
 
 
-var xpath = "//th[normalize-space(text())='Package Dimensions']";
-var xpath2 = "//b[normalize-space(text())='Product Dimensions:']";
+var package_dimensions = "//th[normalize-space(text())='Package Dimensions']";
+var product_dimensions = "//b[normalize-space(text())='Product Dimensions:']";
+var item_weight = "//th[normalize-space(text())='Item Weight']";
+var shiping_weight = "//b[normalize-space(text())='Shipping Weight:']";
 
+var matchingElement = document.evaluate(package_dimensions, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 
-var matchingElement = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 if (matchingElement){
     console.log( "matchingElement.nextSibling.nextSibling.textContent");
 dimensions=matchingElement.nextSibling.nextSibling.textContent;
 dimensions = dimensions.replace(/\s/g,'')
 console.log(dimensions)
-chrome.runtime.sendMessage(dimensions);
 
-}else{//*[@id="detail-bullets"]/table/tbody/tr/td/div[2]/ul/li[1]/b
-    var matchingElement2 = document.evaluate(xpath2, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+
+// **************item weight*************8
+
+var item_weight_xpath = document.evaluate(item_weight, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+var shiping_weight_xpath = document.evaluate(shiping_weight, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+      if (item_weight_xpath)
+        {
+          // console.log("item weight 1******************",item_weight_xpath.nextSibling.nextSibling.textContent)
+          fitem_weight=item_weight_xpath.nextSibling.nextSibling.textContent
+          chrome.runtime.sendMessage(dimensions,fitem_weight);
+        }
+        else
+        {
+          // console.log("shiping weight 1******************",shiping_weight_xpath.nextSibling.textContent)
+          fshiping_weight=shiping_weight_xpath.nextSibling.textContent
+          chrome.runtime.sendMessage(dimensions,fshiping_weight);
+        }
+
+        // chrome.runtime.sendMessage(dimensions);
+}
+else
+{//*[@id="detail-bullets"]/table/tbody/tr/td/div[2]/ul/li[1]/b
+    var matchingElement2 = document.evaluate(product_dimensions, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
     console.log( "ops");
     dimensions2=matchingElement2.nextSibling.textContent;
     // console.log(dimensions2)
     dimensions2 = dimensions2.replace(/\s/g,'')
     console.log(dimensions2)
-    chrome.runtime.sendMessage(dimensions2);
+
+    //***************item_weight */
+
+    var item_weight_xpath = document.evaluate(item_weight, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    var shiping_weight_xpath = document.evaluate(shiping_weight, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    if (item_weight_xpath)
+    {
+      // console.log("item weight ******************",item_weight_xpath.nextSibling.nextSibling.textContent)
+      fitem_weight=item_weight_xpath.nextSibling.nextSibling.textContent
+      chrome.runtime.sendMessage(dimensions2,fitem_weight);
+    }
+    else{
+      // console.log("shiping weight ******************",shiping_weight_xpath.nextSibling.textContent)
+      fshiping_weight=shiping_weight_xpath.nextSibling.textContent
+      chrome.runtime.sendMessage(dimensions2,shiping_weight);
+    }
+    
+    // chrome.runtime.sendMessage(dimensions2);
+
+
+
 }
 
 
